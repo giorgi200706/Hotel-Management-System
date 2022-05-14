d4ad61ce82170c4ab32a3c24fd788d4526ef3b63
giorgi200706 <giorgich31@gmail.com>
2022-05-14 13:27:30 +0400

    Edit_Room class functions is done but  "Edit_Room_modifyroomrecords" not works and everything is commented

--- Edit_Room.cpp
+++ Edit_Room.cpp
@@ -10,6 +10,9 @@ using namespace std;
 
 Edit_Room::Edit_Room(){
 	Edit_Room_num_return = 0;
+	code  = room_no  = status = tariff = save = " ";
+	modify = " ";
+	type = 0;
 }
 int Edit_Room::Edit_Room_printmenu(){
 	
@@ -41,14 +44,102 @@ int Edit_Room::Edit_Room_printmenu(){
 	
 	cout<<endl<<h.center("======================================")<<endl<<endl;
 	if(Edit_Room_num_return == 1){
-		
+		Edit_Room_addroomrecords();
 	}else if(Edit_Room_num_return == 2){
-		
+		Edit_Room_modifyroomrecords();
 	}else if(Edit_Room_num_return == 3){
-		
+		Edit_Room_delteroomrecords();
 	}else if(Edit_Room_num_return == 4){
 		er.Edit_Records_printmenu();
 	}
+}
+
+
+int Edit_Room::Edit_Room_addroomrecords(){
+	
+	// add class subjects
+	Hotel h;
+	Edit_Records er;
+	
+	cout<<h.center("Eter the details for the room")<<endl<<endl
+		<<h.print100("Code	  : ");
+	cin>>code;
+	cout<<h.print100("Room no. : ");
+	cin>>room_no;
+	cout<<h.print100("Status   : ");
+	cin>>status;
+	cout<<h.print100("Tariff   : ");
+	cin>>tariff;
+	cout<<endl;
+	cout<<h.center("do you want to save the record (y/n) : ");
+	
+	for(int i=0; i<1; i=i){
+		cin>>save;
+		if(save == "y" || save == "n"){
+			if(save == "y"){
+				data.push_back(code);
+				data.push_back(room_no);
+				data.push_back(status);
+				data.push_back(tariff);
+			}
+			i = 1;
+			break;
+		}else{
+			cout<<"enter 'y' or 'n' : ";
+		}
+	}
+	cout<<endl<<h.center("======================================")<<endl<<endl;
+	
+	er.Edit_Records_printmenu();
+	
+	code  = room_no = status = tariff = " ";
+	
+}
+
+int Edit_Room::Edit_Room_modifyroomrecords(){
+	
+	// this detail not works
+	/*
+	int k = -1;
+	// add class subjects
+	Hotel h;
+	Edit_Records er;
+	
+	cout<<h.center("Enter the room code whose detail you want to change : ");
+	cin>>modify;
+	for(int i=0; i<data.size(); i = i+4){
+		cout<<i<<"	";
+		if(data[i] == modify){
+			k = i;
+		}
+	}
+	if(k == -1){
+		cout<<"We do not have such a room in the base";
+		Edit_Room_printmenu();
+	}else{
+		cout<<h.center("1. code")<<endl
+			<<h.print100("2. room no.")<<endl
+			<<h.print100("3. status")<<endl
+			<<h.print100("4. tariff")<<endl;
+		cout<<h.center("What detail do you want to change?  ");
+		cin>>type;
+		cout<<"enter a nwe one : ";
+		cin>>data[k+type-1];
+		Edit_Room_printmenu();
+	}
+	*/
+	
+}
+
+int Edit_Room::Edit_Room_delteroomrecords(){
+	
+	// add class subjects
+	Hotel h;
+	data.clear();
+	cout<<h.center("Everything was deleted")<<endl<<endl
+		<<h.center("======================================")<<endl<<endl;
+	Edit_Room_printmenu();
+	
 }
 
 Edit_Room::~Edit_Room(){}
--- Edit_Room.h
+++ Edit_Room.h
@@ -12,10 +12,18 @@ using namespace std;
 class Edit_Room : public Edit_Records{
 	protected:
 		int Edit_Room_num_return;
+		vector<string> data;
+		string code , room_no , status , tariff;
+		string save;
+		string modify;
+		int type;
 		
 	public:
 		Edit_Room();
 		int Edit_Room_printmenu();
+		int Edit_Room_addroomrecords();
+		int Edit_Room_modifyroomrecords();
+		int Edit_Room_delteroomrecords();
 		~Edit_Room();
 };
 
